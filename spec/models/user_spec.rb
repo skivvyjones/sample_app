7127069ad315182850a88642bf093f9205e85a5e
Dave <skivvyjones@gmail.com>
2012-10-22 14:45:50 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,8 +25,14 @@ end
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
   it { should be_valid }
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
   
   describe "when password confirmation is nil" do
   before { @user.password_confirmation = nil }
